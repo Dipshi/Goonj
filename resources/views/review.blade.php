e81e1527c2ed1ecfbf38105a9e6de9b7e4127ec9
Dipshi <2015dipshi.shetty@ves.ac.in>
2018-03-25 22:35:24 +0530

    minor

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -7,10 +7,10 @@
 <input type="text" value="" name="username">
     <input type="email" value="{{session('email')}}" name="email" placeholder="{{session('email')}}"/>
 </span>
-<textarea  name="review"  value="" required></textarea>
+<textarea  name="review"  placeholder="Add Review" value="" required></textarea>
 <b >Rating: </b> <br>
 <fieldset class="rating" >
-    <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label>
+<input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label>
 <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
 <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
 <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
